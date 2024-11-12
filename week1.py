cbc6426fd224b5dbae103cfc85dd3509047f5588
Ojuoye Moshood Olawale <ojuoye1999@gmail.com>
2024-11-12 00:24:29 -0800

    Update week1.py

--- week1.py
+++ week1.py
@@ -1,6 +1,4 @@
-# Simple Calculator Program
 
-# Get user input
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 operation = input("Enter an operation (+, -, *, /): ")
